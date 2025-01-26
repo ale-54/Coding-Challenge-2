589ef2deed337fafbdf6290484476bcd171dbe8a
Ashley Le <ashleyle@usf.edu>
2025-01-26 16:25:14 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+//Task 1 - Store Inventory
+let products = [`Chicken`, `Book`, `Shirt`, `Hand Sanitizer`, `Sticker`];
+products.push(`Wallet`); //adding a new product
+products.pop(); //removing the last product
+console.log(products);
