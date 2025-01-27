201bf4d3d74442669cd99f95e7ceab364c57d0aa
Ashley Le <ashleyle@usf.edu>
2025-01-26 20:52:01 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,11 @@ let products = [`Chicken`, `Book`, `Shirt`, `Hand Sanitizer`, `Sticker`];
 products.push(`Wallet`); //adding a new product
 products.pop(); //removing the last product
 console.log(products);
+
+//Task 2 - Student Scores
+let scores = [12, 87, 64, 0, 37]
+scores[1] = 91;
+let total = scores.reduce((sum, score) => sum + score, 0);
+let averageScore = total / scores.length;
+console.log(`Updated Scores:`, scores);
+console.log(`Average Score:`, averageScore);
